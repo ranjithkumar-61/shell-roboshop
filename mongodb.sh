bbb2249d2290427b5e4d24b6bee49b8c59ebe54b
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-29 23:45:51 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run the script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+
+#$1, $2 argumnets
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+    echo -e "$2 ... $R FAILURE $N " | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$2 ... $G SUCCESS $Y" | tee -a $LOGS_FILE
+fi
+}
+
+
+cp mongo.repo /etc/yum.repo.d/mongo.repo
+VALIDATE $? "Copying Mongo repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Insatalling Mongodb server"
+
+systemctl enable mongod 
+VALIDATE $? "Enable Mongodb"
+
+systemctl start mongod 
+VALIDATE $? "Start Mongodb"
--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,8 @@
 
 SG_ID="sg-0335a843b86ac4387" #security group id
 AMI_ID="ami-0220d79f3f480ecf5"
-
+ZONE_ID="Z05296863N2IX8T55A3ZD"
+DOMAIN_NAME="anreddy.com"
 
 for instance in $@
 do
@@ -21,14 +22,41 @@ do
         --query 'Reservations[].Instances[].PublicIpAddress' \
         --output text
       )
+      RECORD_NAME="$DOMAIN_NAME"  #mangodb.dwas.online
   else
+
     IP=$(
         aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
         --query 'Reservations[].Instances[].PrivateIpAddress' \
         --output text
     )
+    RECORD_NAME="$instance.$DOMAIN_NAME"  #mangodb.dwas.online
+
 fi
 
   echo "IP Adress: $IP"
+aws route53 change-resource-record-sets \
+--hosted-zone-id $ZONE_ID \
+--change-batch '
+  {
+    "Comment": "Updating record",
+    "Changes": [
+      {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+          "Name": "'$RECORD_NAME'",
+          "Type": "A",
+          "TTL": 1,
+          "ResourceRecords": [
+            {
+              "Value": "'$IP'"
+            }
+          ]
+        }
+      }
+    ]
+}
+'
+echo "Record updated for $instance"
 done
